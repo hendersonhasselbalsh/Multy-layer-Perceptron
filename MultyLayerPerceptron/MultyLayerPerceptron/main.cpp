c6a870dd758eeced6f8ebe139bbaed844f166843
HendersonHasselbalsh <wassabi.sashimi@gmail.com>
2024-07-25 20:36:45 -0300

    final configuration

--- MultyLayerPerceptron/MultyLayerPerceptron/main.cpp
+++ MultyLayerPerceptron/MultyLayerPerceptron/main.cpp
@@ -1,7 +1,14 @@
-#include <iostream>
+#include <iostream> 
+#include <opencv2/opencv.hpp> 
 
 
-int main(int argc, const char** argv)
-{
-	std::cout << "hello world\n";
+int main(int argc, char** argv) 
+{ 
+    cv::Mat image = cv::imread("..\\..\\.resources\\test\\0_3.jpg", cv::IMREAD_GRAYSCALE); 
+    cv::imshow("Window Name", image); 
+
+    std::cout << "[SUCESSO]\n"; 
+
+    cv::waitKey(0); 
+    return 0; 
 }
