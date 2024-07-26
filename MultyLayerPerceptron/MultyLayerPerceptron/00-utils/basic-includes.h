128c542265f0954400f3c5fcd5ea59e3580efcf8
HendersonHasselbalsh <wassabi.sashimi@gmail.com>
2024-07-26 09:13:12 -0300

    utils

---
+++ MultyLayerPerceptron/MultyLayerPerceptron/00-utils/basic-includes.h
@@ -0,0 +1,23 @@
+#pragma once
+
+////--------------
+//// usefull libs for inclusion
+////--------------
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <vector>
+#include <filesystem>
+#include <random>
+#include <type_traits>
+#include <cassert>
+#include <fstream>
+#include <utility>
+#include <numeric>
+#include <ranges>
+#include <cfloat>
+
